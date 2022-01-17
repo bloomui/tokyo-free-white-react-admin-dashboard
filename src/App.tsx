cb7fcd3ace880bffbb7b9d185cfb261c078d732b
sandervaneck <69575096+sandervaneck@users.noreply.github.com>
2022-01-17 17:28:03 +0100

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,50 @@
-import { useRoutes } from 'react-router-dom';
-import routes from './router';
-import AdapterDateFns from '@material-ui/lab/AdapterDateFns';
-import LocalizationProvider from '@material-ui/lab/LocalizationProvider';
+import { useRoutes } from "react-router-dom";
+import routes from "./router";
+import AdapterDateFns from "@material-ui/lab/AdapterDateFns";
+import LocalizationProvider from "@material-ui/lab/LocalizationProvider";
 
-import ThemeProvider from './theme/ThemeProvider';
-import { CssBaseline } from '@material-ui/core';
-import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
-import React from 'react';
+import ThemeProvider from "./theme/ThemeProvider";
+import { CssBaseline } from "@material-ui/core";
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import React from "react";
 
-import { setContext } from '@apollo/client/link/context';
-import { getToken, isLoggedIn } from './utilities/auth';
+import { setContext } from "@apollo/client/link/context";
+import { getToken, isLoggedIn } from "./utilities/auth";
 import config from "./config";
 
 const httpLink = createHttpLink();
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = getToken()
+  const token = getToken();
   // return the headers to the context so httpLink can read them
   return {
-    // uri: `http://localhost:8080/graphql/?access_token=accesstoken`,
-    uri: `${config.endpoint}/?access_token=${token}`,
-  }
+    // uri: `http://localhost:8080/graphql?access_token=accesstoken`,
+    uri: `${config.endpoint}?access_token=${token}`,
+  };
 });
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
 
 const App = () => {
-
   const content = useRoutes(routes(isLoggedIn()));
 
   return (
     <ApolloProvider client={client}>
-    <ThemeProvider>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <CssBaseline />
-        {content}
-      </LocalizationProvider>
-    </ThemeProvider>
+      <ThemeProvider>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <CssBaseline />
+          {content}
+        </LocalizationProvider>
+      </ThemeProvider>
     </ApolloProvider>
   );
-}
+};
 export default App;
