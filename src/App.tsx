0b2633f5b8da8a4c94820b3dfb9584e508518a96
sandervaneck <69575096+sandervaneck@users.noreply.github.com>
2022-02-07 10:10:35 +0100

    some updates

--- src/App.tsx
+++ src/App.tsx
@@ -23,9 +23,9 @@ const authLink = setContext((_, { headers }) => {
   const token = getToken();
   // return the headers to the context so httpLink can read them
   return {
-    // uri: `http://localhost:8080/graphql?access_token=accesstoken`,
+    uri: `http://localhost:8080/graphql?access_token=accesstoken`,
     // uri: `http://localhost:8080/graphql?access_token=2116aec7-4a79-36fe-bc44-921b88aac782`,
-    uri: `${config.endpoint}?access_token=${token}`,
+    // uri: `${config.endpoint}?access_token=${token}`,
   };
 });
 
--- src/content/pages/MyChefsbase/Ingredients/AddIngredient/index.tsx
+++ src/content/pages/MyChefsbase/Ingredients/AddIngredient/index.tsx
@@ -1,4 +1,16 @@
-import { Button, Container, Grid, MenuItem, Table, TableCell, TableContainer, TableRow, TextField, TextFieldProps, Typography } from "@material-ui/core";
+import {
+  Button,
+  Container,
+  Grid,
+  MenuItem,
+  Table,
+  TableCell,
+  TableContainer,
+  TableRow,
+  TextField,
+  TextFieldProps,
+  Typography,
+} from "@material-ui/core";
 import { FieldArray, Formik, useField } from "formik";
 import React from "react";
 import { useState } from "react";
@@ -6,12 +18,25 @@ import { Helmet } from "react-helmet-async";
 import { PageHeader } from "src/components/pageHeader/PageHeader";
 import PageTitleWrapper from "src/components/PageTitleWrapper";
 import { FormField } from "src/components/form/FormField";
-import { AddIngredientInput, AddProductInput, AddRecipeInput, NutritionInput, QuantityToId, QuantityToNutritionInput, StepToMethodInput } from "src/globalTypes";
-import { composeValidators, required, Validator } from "src/utilities/formikValidators";
+import {
+  AddIngredientInput,
+  AddProductInput,
+  AddRecipeInput,
+  NutritionInput,
+  QuantityToId,
+  QuantityToNutritionInput,
+  StepToMethodInput,
+} from "src/globalTypes";
+import {
+  composeValidators,
+  required,
+  Validator,
+} from "src/utilities/formikValidators";
 import { user } from "../..";
 import { Rating1 } from "../../Menus/filtermenus/components/rating";
 import { useAddIngredient } from "../api";
-import { Divider } from '@mui/material';import { AddIngredientVariables } from "../types/AddIngredient";
+import { Divider } from "@mui/material";
+import { AddIngredientVariables } from "../types/AddIngredient";
 import { TableProductData } from "./component/ProductsTable";
 import { FormikSelect } from "src/components/form/FormikSelect";
 import { H3, H5, H5Left } from "src/content/pages/Components/TextTypes";
@@ -27,56 +52,57 @@ export const emptyNutrition: NutritionInput = {
   fatstotal: 0,
   carbscarbs: 0,
   carbssugar: 0,
-  fibres: 0
-}
+  fibres: 0,
+};
 export const emptyQuantityToNutrition: QuantityToNutritionInput = {
   quantity: 0,
-  unit: '',
-  nutrition: emptyNutrition
-}
+  unit: "",
+  nutrition: emptyNutrition,
+};
 
 export const AddIngredientPage = () => {
-  const [ dialog, openDialog] = useState(false)
+  const [dialog, openDialog] = useState(false);
 
-    const { addIngredient, loading, error } = useAddIngredient({
-        onCompleted: () => {window.location.reload()},
-        },
-      );
-      const navigate =  useNavigate()
-      
-      const [selectedProducts, setProducts] = React.useState<productToQ[]>([]);
+  const { addIngredient, loading, error } = useAddIngredient({
+    onCompleted: () => {
+      window.location.reload();
+    },
+  });
+  const navigate = useNavigate();
 
-      const formInput: AddIngredientInput = {
-        name: '',
-        rating: 0,
-        category: '',
-        nutrition: emptyQuantityToNutrition,
-      }
+  const [selectedProducts, setProducts] = React.useState<productToQ[]>([]);
 
-            function handleDelete(index) {
-                selectedProducts.splice(index, 1)
-                setProducts([...selectedProducts])
-            }
-    
-    const formProducts: string[] | null = []
-        
-    
-    const formState : AddIngredientVariables = {
-        input: formInput,
-        products: formProducts,
-    }
-        
-    return (
-        <>
-        <Helmet>
+  const formInput: AddIngredientInput = {
+    name: "",
+    rating: 0,
+    category: "",
+    nutrition: emptyQuantityToNutrition,
+    material: "SOLID",
+  };
+
+  function handleDelete(index) {
+    selectedProducts.splice(index, 1);
+    setProducts([...selectedProducts]);
+  }
+
+  const formProducts: string[] | null = [];
+
+  const formState: AddIngredientVariables = {
+    input: formInput,
+    products: formProducts,
+  };
+
+  return (
+    <>
+      <Helmet>
         <title>Nieuw Ingredient</title>
       </Helmet>
       <PageTitleWrapper>
-      <PageHeader
-        title="Nieuw Ingredient"
-        name="Soup Bros"
-        avatar={user.avatar}
-         />
+        <PageHeader
+          title="Nieuw Ingredient"
+          name="Soup Bros"
+          avatar={user.avatar}
+        />
       </PageTitleWrapper>
       <Container maxWidth="lg">
         <Grid
@@ -87,154 +113,166 @@ export const AddIngredientPage = () => {
           spacing={3}
         >
           <Grid item xs={12}>
-        <Formik
-        initialValues={formState}
-        onSubmit={(values) => {
-            addIngredient({
-            variables: {
-                products: selectedProducts.map((productToId) => productToId.id),
-                input: {
-                name: values.input.name,
-                rating: values.input.rating,
-                category: values.input.category,
-                nutrition: values.input.nutrition              
-            },
-            },
-          });
-        }}
-      >
-        {({ values, handleChange, submitForm, setFieldValue }) => {
-          return (
-            <>
-            <Grid container xs={12} spacing={2}>
-                <Grid xs={3}>
-                <H5 title="Ingredientnaam"/>
-                <FormField
-                  name="input.name"
-                  label="Naam"
-                  validator={composeValidators(required)}
-                />
-                </Grid>
-                <Grid xs={1}></Grid>
-                <Grid xs={3}>
-                <H5 title="Categorie"/>
-                <FormField
-                  name="input.category"
-                  label="Categorie"
-                />
-                </Grid> 
-                <Grid xs={4}></Grid>
-                <Grid xs={3}>
-                <Rating1
-                updateField="input.rating"
-                setFieldValue={setFieldValue}
-                />
-                </Grid> 
-                <Grid xs={1}></Grid>
-                <Grid xs={3}>
-                <H5 title="Toevoegen"/>
-                <Button
-                  disabled={loading}
-                  onClick={() => submitForm()}
-                  color="primary"
-                  variant="contained"
-                >
-                  Gegevens toevoegen
-                </Button>
-                </Grid>               
-                </Grid>
-                <Divider/>
-                <Grid xs={12}>
-                <InsertNutrition
-                setFieldValue={setFieldValue}
-                />
+            <Formik
+              initialValues={formState}
+              onSubmit={(values) => {
+                addIngredient({
+                  variables: {
+                    products: selectedProducts.map(
+                      (productToId) => productToId.id
+                    ),
+                    input: {
+                      name: values.input.name,
+                      rating: values.input.rating,
+                      category: values.input.category,
+                      nutrition: values.input.nutrition,
+                    },
+                  },
+                });
+              }}
+            >
+              {({ values, handleChange, submitForm, setFieldValue }) => {
+                return (
+                  <>
+                    <Grid container xs={12} spacing={2}>
+                      <Grid xs={3}>
+                        <H5 title="Ingredientnaam" />
+                        <FormField
+                          name="input.name"
+                          label="Naam"
+                          validator={composeValidators(required)}
+                        />
+                      </Grid>
+                      <Grid xs={1}></Grid>
+                      <Grid xs={3}>
+                        <H5 title="Categorie" />
+                        <FormField name="input.category" label="Categorie" />
+                      </Grid>
+                      <Grid xs={4}></Grid>
+                      <Grid xs={3}>
+                        <H5 title="Soort" />
+                        <FormikSelect validate={required} name="input.material">
+                          {["In gram", "In liters", "In stuks"].map(
+                            (material) => (
+                              <MenuItem value={material} key={material}>
+                                {material}
+                              </MenuItem>
+                            )
+                          )}
+                        </FormikSelect>
+                      </Grid>
+                      <Grid xs={4}></Grid>
+                      <Grid xs={3}>
+                        <Rating1
+                          updateField="input.rating"
+                          setFieldValue={setFieldValue}
+                        />
+                      </Grid>
+                      <Grid xs={1}></Grid>
+                      <Grid xs={3}>
+                        <H5 title="Toevoegen" />
+                        <Button
+                          disabled={loading}
+                          onClick={() => submitForm()}
+                          color="primary"
+                          variant="contained"
+                        >
+                          Gegevens toevoegen
+                        </Button>
+                      </Grid>
                     </Grid>
-                <Divider/>
-                <Grid container xs={12}>
-                  <Grid xs={12}>
-                <H3 title="Productopties"/>
-                </Grid>
-                <Grid xs={12}>
-                </Grid>
-                <Grid xs={6}>
-                  <Grid xs={12}>
-                <Button
-                  // onClick={() => navigate("/mychefsbase/addingredient")}
-                  onClick={() => openDialog(true)}
-                  color="primary"
-                  variant="contained"
-                >
-                  Snel producten toevoegen
-                </Button>
-                </Grid>
-                <AddQuickProductsDialog
-                open={dialog}
-                onClose={() => openDialog(false)} />
-                  <TableProductData 
-                  setProduct={(selected) => setProducts([...selectedProducts, selected])
-                  }/>
-                  </Grid>
-                <Grid xs={6}>
-                  <TableContainer>
-                <Table size="small">
-                  <TableRow>
-                    <TableCell>Naam</TableCell>
-                    <TableCell>email</TableCell>
-                    <TableCell>Herkomst</TableCell>
-                    <TableCell>Prijs</TableCell>
-                    </TableRow>
-                {selectedProducts.map((product, index) =>  (
-                  <TableRow>
-                    <TableCell>
-                      {product.name}
-                    </TableCell>
-                    <TableCell>
-                      {product.brand}
-                    </TableCell>
-                    <TableCell>
-                      {product.origin}
-                    </TableCell>
-                    <TableCell>
-                      {product.price}
-                    </TableCell>
-                    <TableCell>
-                    <Button
-                       variant="contained" 
-                       color="secondary"
-                        style={{maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px'}} type="button" 
-                         onClick={() => {handleDelete(index)}}>           
-                                                 -
-                       </Button>
-                    </TableCell>
-                  </TableRow>
-                ))}
-                </Table>
-                </TableContainer>
-                </Grid>
-                </Grid>             
-                {error && (
-                  <Typography color="error">
-                    Er is een fout opgetreden, probeer het opnieuw.
-                  </Typography>
-                )}
-            </>
-          );
-        }}
-      </Formik>
-      </Grid>
+                    <Divider />
+                    <Grid xs={12}>
+                      <InsertNutrition setFieldValue={setFieldValue} />
+                    </Grid>
+                    <Divider />
+                    <Grid container xs={12}>
+                      <Grid xs={12}>
+                        <H3 title="Productopties" />
+                      </Grid>
+                      <Grid xs={12}></Grid>
+                      <Grid xs={6}>
+                        <Grid xs={12}>
+                          <Button
+                            // onClick={() => navigate("/mychefsbase/addingredient")}
+                            onClick={() => openDialog(true)}
+                            color="primary"
+                            variant="contained"
+                          >
+                            Snel producten toevoegen
+                          </Button>
+                        </Grid>
+                        <AddQuickProductsDialog
+                          open={dialog}
+                          onClose={() => openDialog(false)}
+                        />
+                        <TableProductData
+                          setProduct={(selected) =>
+                            setProducts([...selectedProducts, selected])
+                          }
+                        />
+                      </Grid>
+                      <Grid xs={6}>
+                        <TableContainer>
+                          <Table size="small">
+                            <TableRow>
+                              <TableCell>Naam</TableCell>
+                              <TableCell>email</TableCell>
+                              <TableCell>Herkomst</TableCell>
+                              <TableCell>Prijs</TableCell>
+                            </TableRow>
+                            {selectedProducts.map((product, index) => (
+                              <TableRow>
+                                <TableCell>{product.name}</TableCell>
+                                <TableCell>{product.brand}</TableCell>
+                                <TableCell>{product.origin}</TableCell>
+                                <TableCell>{product.price}</TableCell>
+                                <TableCell>
+                                  <Button
+                                    variant="contained"
+                                    color="secondary"
+                                    style={{
+                                      maxWidth: "30px",
+                                      maxHeight: "30px",
+                                      minWidth: "30px",
+                                      minHeight: "30px",
+                                    }}
+                                    type="button"
+                                    onClick={() => {
+                                      handleDelete(index);
+                                    }}
+                                  >
+                                    -
+                                  </Button>
+                                </TableCell>
+                              </TableRow>
+                            ))}
+                          </Table>
+                        </TableContainer>
+                      </Grid>
+                    </Grid>
+                    {error && (
+                      <Typography color="error">
+                        Er is een fout opgetreden, probeer het opnieuw.
+                      </Typography>
+                    )}
+                  </>
+                );
+              }}
+            </Formik>
+          </Grid>
         </Grid>
       </Container>
-      </>
-          )
-}
+    </>
+  );
+};
 
 export type productToQ = {
-  name: string,
-  id: string,
-  brand: string,
-  origin: string,
-  price: number,
-  quantity: number,
-  unit: string,
-}
-
+  name: string;
+  id: string;
+  brand: string;
+  origin: string;
+  price: number;
+  quantity: number;
+  unit: string;
+};
